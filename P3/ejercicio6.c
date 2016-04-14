04fad919373ccf2ed2106740b8a9c58a67a775de
angel-manuel <angel_manuel_martin@yahoo.es>
2016-04-14 18:38:07 +0200

    Ejercicio 6 working

--- P3/ejercicio6.c
+++ P3/ejercicio6.c
@@ -1,11 +1,23 @@
 /**
- * @file ejercicio6.c 
- * @brief 
+ * @file ejercicio6.c
+ * @brief
 
  * @author Darío Adrián Hernández
- * @author Ángel Manuel Martín 
+ * @author Ángel Manuel Martín
  * @date 2016/04/06
-*/
+ */
+
+ #include <stdio.h>
+ #include <sys/types.h>
+ #include <sys/wait.h>
+ #include <sys/ipc.h>
+ #include <string.h>
+ #include <errno.h>
+ #include <sys/shm.h>
+ #include <stdlib.h>
+ #include <signal.h>
+ #include <unistd.h>
+ #include <pthread.h>
 
 #include "semaforos.h"
 
@@ -14,16 +26,17 @@
 #define SEMA_GLO 0
 #define SEMA_IZQ 1
 #define SEMA_DER 2
-#define NUM_HILOS 10
+#define NUM_PROCESOS 100
+#define SLEEP_MAXIMO 20
 
 /*Variables para la memoria compartida*/
 #define FILEKEY "/bin/cat"
-#define KEY 666
+#define KEY 888
 
 void coche(int id, int semid, int* flujo_de_coches, int sentido) {
     int sema_dir = sentido == -1 ? SEMA_IZQ : SEMA_DER;
 
-    sleep(rand() % 10);
+    sleep(rand() % SLEEP_MAXIMO);
 
     Down_Semaforo(semid, sema_dir, 1);
 
@@ -54,7 +67,7 @@ void coche(int id, int semid, int* flujo_de_coches, int sentido) {
 
 
 void* nuevo_proceso() {
-	int semid, *flujo_de_coches, key, id_zone;
+    int semid, *flujo_de_coches, key, id_zone;
 
     int id = getpid();
 
@@ -70,19 +83,19 @@ void* nuevo_proceso() {
         exit(0);
     }
 
-    id_zone = shmget(key, sizeof(int*), IPC_EXCL | SHM_R | SHM_W);
+    id_zone = shmget(key, sizeof(int*), SHM_R | SHM_W);
     if (id_zone == -1) {
         fprintf(stderr, "Error with shmget 2\n");
         exit(0);
     }
-        
-	flujo_de_coches = (int*)shmat(id_zone, NULL, 0);
+
+    flujo_de_coches = (int*)shmat(id_zone, NULL, 0);
     if (flujo_de_coches == NULL) {
         fprintf(stderr, "Error with shmat\n");
         exit(0);
-    }        
+    }
 
-    coche(id, semid, &flujo_de_coches, id % 2 ? +1 : -1);
+    coche(id, semid, flujo_de_coches, id % 2 ? +1 : -1);
 
     /*Salir de la memoria compartida*/
     shmdt(flujo_de_coches);
@@ -93,6 +106,7 @@ void* nuevo_proceso() {
 int main() {
     int semid, id_zone, key, *flujo_de_coches;
     int i;
+    unsigned short init_arr[3] = {1, 1, 1};
 
     key = ftok(FILEKEY, KEY);
     if (key == -1) {
@@ -108,7 +122,9 @@ int main() {
 
     if (Crear_Semaforo(SEMKEY, 3, &semid) == ERROR) {
         return 1;
-    }   
+    }
+
+    Inicializar_Semaforo(semid, init_arr);
 
     flujo_de_coches = (int*)shmat(id_zone, NULL, 0);
     if (flujo_de_coches == NULL) {
@@ -116,19 +132,24 @@ int main() {
         exit(0);
     }
 
-    for (i = 0;i < 100 ;++i) {
-    	if (!fork()){
-    		nuevo_proceso();
-    	}
+    *flujo_de_coches = 0;
+
+    for (i = 0; i < NUM_PROCESOS; ++i) {
+        if (!fork()) {
+            nuevo_proceso();
+        }
     }
 
-	for (i = 0;i < 100;++i){
-		wait(0);
-	}
+    for (i = 0; i < NUM_PROCESOS; ++i) {
+        wait(0);
+    }
 
     if (Borrar_Semaforo(semid) == -1) {
         return 1;
-    }	
+    }
+
+    shmdt(flujo_de_coches);
+    shmctl(id_zone, IPC_RMID, 0);
 
     return 0;
 }
