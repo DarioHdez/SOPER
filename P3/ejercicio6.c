697f3c6e50f77f88e332bc59156a6016ee74c317
Dariohdez <dariohdez16@gmail.com>
2016-04-14 17:41:14 +0200

    Iniciando ejercicio6

--- P3/ejercicio6.c
+++ P3/ejercicio6.c
@@ -9,20 +9,116 @@
 
 #include "semaforos.h"
 
+/*Variables para los semaforos*/
 #define SEMKEY 66666
+#define SEMA_GLO 0
+#define SEMA_IZQ 1
+#define SEMA_DER 2
+#define NUM_HILOS 10
 
-int coche() {
+/*Variables para la memoria compartida*/
+#define FILEKEY "/bin/cat"
+#define KEY 666
 
+void coche(int id, int semid, int* flujo_de_coches, int sentido) {
+    int sema_dir = sentido == -1 ? SEMA_IZQ : SEMA_DER;
+
+    sleep(rand() % 10);
+
+    Down_Semaforo(semid, sema_dir, 1);
+
+    printf("%d: Entrando de puente con sentido %d\n", id, sentido);
+
+    if ((*flujo_de_coches)*sentido <= 0) { // Si no estan pasando o pasan en el otro sentido
+        Down_Semaforo(semid, SEMA_GLO, 1); // espero a establecer mi sentido
+    }
+
+    *flujo_de_coches += sentido;
+
+    Up_Semaforo(semid, sema_dir, 1);
+
+    printf("%d: En el puente con sentido %d\nflujo_de_coches = %d\n", id, sentido, *flujo_de_coches);
+    sleep(1);
+
+    Down_Semaforo(semid, sema_dir, 1);
+
+    *flujo_de_coches -= sentido;
+    printf("%d: Saliendo de puente con sentido %d\n", id, sentido);
+
+    if (*flujo_de_coches == 0) {
+        Up_Semaforo(semid, SEMA_GLO, 1);
+    }
+
+    Up_Semaforo(semid, sema_dir, 1);
 }
 
-int main() {
-    int semid, id_zone;
 
-    if (Crear_Semaforo(SEMKEY, 1, &semid) == ERROR) {
+void* nuevo_proceso() {
+	int semid, flujo_de_coches, key id_zone;
+
+    int id = getpid();
+    /*Añadir la memoria compartida*/
+
+    key = ftok(FILEKEY, KEY);
+    if (key == -1) {
+        fprintf(stderr, "Error with key\n");
+        exit(0);
+    }
+
+    id_zone = shmget(key, sizeof(info_t), | IPC_EXCL | SHM_R | SHM_W);
+    if (id_zone == -1) {
+        fprintf(stderr, "Error with shmget\n");
+        exit(0);
+
+	flujo_de_coches = (int*)shmat(id_zone, NULL, 0);
+    if (flujo_de_coches == NULL) {
+        fprintf(stderr, "Error with shmat\n");
+        exit(0);
+    }        
+
+    coche(id, semid, &flujo_de_coches, id % 2 ? +1 : -1);
+
+    /*Salir de la memoria compartida*/
+    shmdt(my_info);
+
+    pthread_exit(NULL);
+}
+
+int main(int ) {
+    int semid, id_zone, key, flujo_de_coches;
+    int i;
+
+    key = ftok(FILEKEY, KEY);
+    if (key == -1) {
+        fprintf(stderr, "Error with key\n");
+        exit(0);
+    }
+
+    id_zone = shmget(key, sizeof(info_t), IPC_CREAT | IPC_EXCL | SHM_R | SHM_W);
+    if (id_zone == -1) {
+        fprintf(stderr, "Error with shmget\n");
+        exit(0);
+    }
+
+    if (Crear_Semaforo(SEMKEY, 3, &semid) == ERROR) {
         return 1;
     }   
 
+    flujo_de_coches = (int*)shmat(id_zone, NULL, 0);
+    if (flujo_de_coches == NULL) {
+        fprintf(stderr, "Error with shmat\n");
+        exit(0);
+    }
 
+    for (i = 0;i < 100 ;++i) {
+    	if (!fork()){
+    		nuevo_proceso();
+    	}
+    }
+
+	for (i = 0;i < 100;++i){
+		wait(0);
+	}
 
     if (Borrar_Semaforo(semid) == -1) {
         return 1;
